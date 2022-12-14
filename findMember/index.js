794fc2f3cd983a8f880cdef80ee181c499ba578d
Joe Marcus <josephcmarcus@gmail.com>
2022-12-14 14:15:13 -0500

    Create findMember function to search for member in Mailchimp audience

---
+++ findMember/index.js
@@ -0,0 +1,34 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ *
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+require("dotenv").config();
+const client = require("@mailchimp/mailchimp_marketing");
+
+client.setConfig({
+  apiKey: process.env.MC_KEY,
+  server: process.env.MC_SERVER,
+});
+
+module.exports = async function (context) {
+  // Obtain the listId and email from the context object
+  const listId = context.bindingData.name[0];
+  const email = context.bindingData.name[1].shift().object.user.email;
+
+  try {
+    // Use the mailchimp client to see if a member exists in the list
+    const response = await client.lists.getListMember(listId, email);
+    // if the member exists, return the response
+    return response;
+  } catch (err) {
+    // If the member doesn't exist, return null
+    return null;
+  }
+};
---
+++ httpStarter/index.js
@@ -0,0 +1,14 @@
+﻿const df = require("durable-functions");
+
+module.exports = async function (context, req) {
+    const client = df.getClient(context);
+    const instancePayload = {
+        "body": req.body,
+        "listId": req.query.listId
+    }
+    const instanceId = await client.startNew(req.params.functionName, undefined, instancePayload);
+
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+    return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+};
